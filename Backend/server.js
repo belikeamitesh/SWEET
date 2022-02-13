26e1c87229f0c683bf1cd6f68b6abdcc92c8d337
Majid Djido <abdoulmajid.djido@yahoo.com>
2022-02-13 05:38:41 -0500

    centralisation index/server

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,35 @@
-const express = require('express')
-const app = express()
-const portNumber = 5000
+const express = require("express");
+const Account = require("./twilioAccountInfo.json");
+const app = express();
+const portNumber = 5000;
 
-app.get("/api", (req, res)=>{
-    res.json({"users" : ["userOne", "userTwo", "userThree"]});
-})
+app.listen(portNumber, () => {
+  console.log("Server listen on port " + portNumber);
+});
 
-app.listen(portNumber, ()=>{console.log("Server listen on port " + portNumber)})
+const client = require("twilio")(Account.accountSid, Account.authToken, {
+  lazyLoading: true,
+});
+
+function sendSMS() {
+  client.messages
+    .create({
+      body: "A woman requires your assistance.",
+      to: "+14388242772", // Text this number
+      from: "+18126338917", // From a valid Twilio number
+    })
+    .then((message) => console.log(message.sid));
+}
+
+app.post("/sendSMS", (req, res) => {
+  console.log("request for SMS");
+  let user = req.body;
+  sendSMS(user, (info) => {
+    console.log(`The SMS has been send 😃 and the id is ${info.messageId}`);
+    res.send(info);
+  });
+});
+
+app.get("/api", (req, res) => {
+  res.json({ users: ["userOne", "userTwo", "userThree"] });
+});
